00e242b355c94c1ee123e2ff0abb733aeb71d0db
Prince Chauhan <princechauhan546@gmail.com>
2023-03-24 16:13:49 +0530

    change

--- src/fetchPet.js
+++ src/fetchPet.js
@@ -1,5 +1,5 @@
 const fetchPet = async ({ queryKey }) => {
-  console.log("queryKey", queryKey);
+  // console.log("queryKey", queryKey);
   const id = queryKey[1];
 
   const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
--- src/fetchSearch.js
+++ src/fetchSearch.js
@@ -1,5 +1,5 @@
 async function fetchSearch({ queryKey }) {
-    console.log(queryKey)
+  // console.log(queryKey)
   const { animal, location, breed } = queryKey[1];
   const res = await fetch(
     `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
@@ -10,6 +10,5 @@ async function fetchSearch({ queryKey }) {
   }
   return res.json();
 }
-
 
 export default fetchSearch;
