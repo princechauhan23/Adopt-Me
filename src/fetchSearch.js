4b5a869bbdd221fca730f6e7afa9dd412e57ec64
Prince Chauhan <princechauhan546@gmail.com>
2023-04-03 00:23:25 +0530

    check error

--- src/fetchSearch.js
+++ src/fetchSearch.js
@@ -1,14 +1,19 @@
 async function fetchSearch({ queryKey }) {
+  try {
   // console.log(queryKey)
   const { animal, location, breed } = queryKey[1];
   const res = await fetch(
     `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
   );
 
+  console.log("res", res)
   if (!res.ok) {
     throw new Error(`pet search not okay ${animal}, ${location}, ${breed}`);
   }
   return res.json();
+}catch (error) {
+  console.log("error",error)
+}
 }
 
 export default fetchSearch;
